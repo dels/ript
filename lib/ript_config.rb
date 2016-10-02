a9ef64cabf7a0dcc2c2ca5eafde7a2b8bca00ac2
root <root@srv19.elsbroek.com>
2016-10-02 13:45:46 +0200

    corrected path of configuration file

--- lib/ript_config.rb
+++ lib/ript_config.rb
@@ -5,9 +5,9 @@ require "json"
 class RIPTConfig
 
   def initialize(cfg=nil)
-    cfg ||= "./ript.json"
+    cfg ||= "#{Dir.pwd}/ript.json"
     # load config file
-    raise "configuration file must be readable" unless File.exist? cfg and File.file? cfg and File.readable? cfg
+    raise "configuration file must be readable (#{cfg})" unless File.exist? cfg and File.file? cfg and File.readable? cfg
     raise "could not load configuration file" unless (@fw = JSON.parse(File.read(cfg)))
   end
   
